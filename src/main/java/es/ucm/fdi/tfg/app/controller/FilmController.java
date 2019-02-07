95ae775cba879bcf928524e3ba92120c50a6af67
Dacuna <dacuna@ucm.es>
2019-02-07 11:31:31 +0100

    minor changes

--- src/main/java/es/ucm/fdi/tfg/app/controller/FilmController.java
+++ src/main/java/es/ucm/fdi/tfg/app/controller/FilmController.java
@@ -6,6 +6,7 @@ import com.github.lambdaexpression.annotation.EnableRequestBodyParam;
 import com.github.lambdaexpression.annotation.RequestBodyParam;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,6 @@ public class FilmController {
     public Optional<Film> getFilmById(@PathVariable String id) {
         return filmRepository.findById(id);
     }
+
 
 }
