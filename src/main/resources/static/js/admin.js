6862894a493ed83db30b8b0ed190ca2e96c17aa5
ZReKoJ <zhong.9745@gmail.com>
2019-02-10 21:59:29 +0100

    set database initialization to update

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,11 +1,15 @@
 $(() => {
 
+    console.log(data);
+
     data.forEach(element => {
         $.ajax({
             method: "GET",
             url: "/film/" + element,
             success: function (data, textStatus, jqXHR) {
-                console.log(data);
+                if (data) {
+                    console.log(data);
+                }
             },
             // En caso de error, mostrar el error producido
             error: function (jqXHR, textStatus, errorThrown) {
