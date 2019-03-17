d2c6ab3f6cece04bd853873097242a80eee62b41
danie <danie@DESKTOP-DGVHJPT>
2019-03-17 11:57:12 +0100

    Heroku data is changed

--- src/main/java/es/ucm/fdi/tfg/app/controller/FriendshipController.java
+++ src/main/java/es/ucm/fdi/tfg/app/controller/FriendshipController.java
@@ -1,7 +1,5 @@
 package es.ucm.fdi.tfg.app.controller;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -16,12 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import es.ucm.fdi.tfg.app.entity.Film;
 import es.ucm.fdi.tfg.app.entity.Friendship;
 import es.ucm.fdi.tfg.app.entity.FriendshipId;
 import es.ucm.fdi.tfg.app.entity.UserApp;
