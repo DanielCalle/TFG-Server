9a78f976ac34fb10ae6c5bb654c55e11d9b3ab85
ZReKoJ <zhong.9745@gmail.com>
2019-04-09 17:14:26 +0200

    user fixed with mapper

--- src/main/java/es/ucm/fdi/tfg/app/sa/SAUserImp.java
+++ src/main/java/es/ucm/fdi/tfg/app/sa/SAUserImp.java
@@ -3,7 +3,11 @@ package es.ucm.fdi.tfg.app.sa;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +22,13 @@ import es.ucm.fdi.tfg.app.transfer.TFriendship;
 import es.ucm.fdi.tfg.app.transfer.TPlan;
 import es.ucm.fdi.tfg.app.transfer.TUser;
 
-
 @Service
-public class SAUserImp implements SAUser{
+public class SAUserImp implements SAUser {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	private ModelMapper modelMapper = new ModelMapper();
 
 	@Override
 	public TUser create(TUser tUser) {
@@ -36,8 +41,7 @@ public class SAUserImp implements SAUser{
 			user.setPassword(tUser.getPassword());
 			user.setImageURL(tUser.getImageURL());
 			user = userRepository.save(user);
-
-			return tUser;
+			return modelMapper.map(user, TUser.class);
 		}
 
 		return null;
@@ -46,21 +50,20 @@ public class SAUserImp implements SAUser{
 	@Override
 	public TUser update(TUser tUser) {
 		Optional<UserApp> optUser = userRepository.findById(tUser.getUuid());
-    	if (optUser.isPresent()) {
+
+		if (optUser.isPresent()) {
 			UserApp user = optUser.get();
 			user.setUuid(tUser.getUuid());
 			user.setName(tUser.getName());
 			user.setEmail(tUser.getEmail());
 			user.setPassword(tUser.getPassword());
 			user.setImageURL(tUser.getImageURL());
-			
 
 			user = userRepository.save(user);
-    		
-    		return tUser;
-    	}
-    	
-    	return null;
+			return modelMapper.map(user, TUser.class);
+		}
+
+		return null;
 	}
 
 	@Override
@@ -69,129 +72,73 @@ public class SAUserImp implements SAUser{
 
 		if (optUser.isPresent()) {
 			userRepository.delete(optUser.get());
-			
 			return uuid;
 		}
-		
+
 		return null;
 	}
 
 	@Override
 	public TUser read(String uuid) {
 		Optional<UserApp> optUser = userRepository.findById(uuid);
-		
+
 		if (optUser.isPresent()) {
-			TUser tUser = new TUser();
-			tUser.setUuid(optUser.get().getUuid());
-			tUser.setName(optUser.get().getName());
-			tUser.setEmail(optUser.get().getEmail());
-			tUser.setPassword(optUser.get().getPassword());
-			tUser.setImageURL(optUser.get().getImageURL());
-			
-			return tUser;
+			return modelMapper.map(optUser.get(), TUser.class);
 		}
-		
+
 		return null;
 	}
 
 	@Override
 	public List<TUser> readAll() {
-		List<TUser> listTUser = new ArrayList<>();
 		Iterable<UserApp> listUserApp = userRepository.findAll();
-		
-		for (UserApp userApp : listUserApp) {
-			TUser tUser = new TUser();
-			tUser.setUuid(userApp.getUuid());
-			tUser.setName(userApp.getName());
-			tUser.setEmail(userApp.getEmail());
-			tUser.setPassword(userApp.getPassword());
-			tUser.setImageURL(userApp.getImageURL());
-			listTUser.add(tUser);
-		}
-		
-		return listTUser;
+
+		return StreamSupport.stream(listUserApp.spliterator(), false)
+			.map(user -> modelMapper.map(user, TUser.class))
+			.collect(Collectors.toList());
 	}
 
 	@Override
 	public List<TFriendship> getFriends(String uuid) {
 		Optional<UserApp> optUser = userRepository.findById(uuid);
-		
+
 		if (optUser.isPresent()) {
-			List<TFriendship> listFriends = new ArrayList<>();
-			for (Friendship friendship : optUser.get().getFriendRequests()) {
-				TFriendship tFriendship = new TFriendship();
-				tFriendship.setRequesterUuid(friendship.getRequester().getUuid());
-				tFriendship.setFriendUuid(friendship.getFriend().getUuid());
-				tFriendship.setDate(friendship.toString());
-				listFriends.add(tFriendship);
-			}
-			for (Friendship friendship : optUser.get().getFriends()) {
-				TFriendship tFriendship = new TFriendship();
-				tFriendship.setRequesterUuid(friendship.getRequester().getUuid());
-				tFriendship.setFriendUuid(friendship.getFriend().getUuid());
-				tFriendship.setDate(friendship.toString());
-				listFriends.add(tFriendship);
-			}
-			
-			return listFriends;
+			return Stream.concat(
+				optUser.get().getFriendRequests().stream()
+					.map(friendShip -> modelMapper.map(friendShip, TFriendship.class)),
+					optUser.get().getFriends().stream()
+					.map(friendShip -> modelMapper.map(friendShip, TFriendship.class))
+			).distinct().collect(Collectors.toList());
 		}
-		
+
 		return null;
 	}
 
 	@Override
 	public List<TPlan> getPlans(String uuid) {
 		Optional<UserApp> optUser = userRepository.findById(uuid);
-		
+
 		if (optUser.isPresent()) {
-			List<TPlan> listPlans = new ArrayList<>();
-			for (Plan plan : optUser.get().getCreatedPlans()) {
-				TPlan tPlan = new TPlan();
-				tPlan.setId(plan.getId());
-				tPlan.setCreatorUuid(plan.getCreator().getUuid());
-				tPlan.setFilmUuid(plan.getFilm().getUuid());
-				tPlan.setDate(plan.getDate().toString());
-				listPlans.add(tPlan);
-			}
-			for (Plan plan : optUser.get().getJoinedPlans()) {
-				TPlan tPlan = new TPlan();
-				tPlan.setId(plan.getId());
-				tPlan.setCreatorUuid(plan.getCreator().getUuid());
-				tPlan.setFilmUuid(plan.getFilm().getUuid());
-				tPlan.setDate(plan.getDate().toString());
-				listPlans.add(tPlan);
-			}
-			
-			return listPlans;
+			return Stream.concat(
+				optUser.get().getCreatedPlans().stream()
+					.map(plan -> modelMapper.map(plan, TPlan.class)),
+				optUser.get().getJoinedPlans().stream()
+					.map(plan -> modelMapper.map(plan, TPlan.class))
+			).distinct().collect(Collectors.toList());
 		}
-		
+
 		return null;
 	}
 
 	@Override
 	public List<TFilm> getFilms(String uuid) {
 		Optional<UserApp> optUser = userRepository.findById(uuid);
-		
+
 		if (optUser.isPresent()) {
-			List<TFilm> listUserFilms = new ArrayList<>();
-			for (UserFilm userFilm : optUser.get().getUserFilms()) {
-				TFilm tFilm = new TFilm();
-				tFilm.setUuid(userFilm.getFilm().getUuid());
-				tFilm.setCountry(userFilm.getFilm().getCountry());
-				tFilm.setDirector(userFilm.getFilm().getDirector());
-				tFilm.setDuration(userFilm.getFilm().getDuration());
-				tFilm.setGenre(userFilm.getFilm().getGenre());
-				tFilm.setImageURL(userFilm.getFilm().getImageURL());
-				tFilm.setInfoURL(userFilm.getFilm().getInfoURL());
-				tFilm.setName(userFilm.getFilm().getName());
-				tFilm.setRating(userFilm.getFilm().getRating());
-				tFilm.setTrailerURL(userFilm.getFilm().getTrailerURL());
-				listUserFilms.add(tFilm);
-			}
-			
-			return listUserFilms;
+			return optUser.get().getUserFilms().stream()
+					.map(userFilm -> modelMapper.map(userFilm.getFilm(), TFilm.class)).collect(Collectors.toList());
 		}
-		
+
 		return null;
 	}
 
