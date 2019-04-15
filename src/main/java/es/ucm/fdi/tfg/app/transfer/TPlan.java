ee1058553a997476274b8690a9adf01ff316e988
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-04-15 18:27:40 +0200

    joinedUsers added to tPlan

--- src/main/java/es/ucm/fdi/tfg/app/transfer/TPlan.java
+++ src/main/java/es/ucm/fdi/tfg/app/transfer/TPlan.java
@@ -1,12 +1,15 @@
 package es.ucm.fdi.tfg.app.transfer;
 
 import java.util.Date;
+import java.util.List;
 
 public class TPlan {
 	private Long id;
 	private String creatorUuid;
 	private String filmUuid;
 	private Date date;
+	private List<String> joinedUsers;
+
 	public String getCreatorUuid() {
 		return creatorUuid;
 	}
@@ -30,6 +33,12 @@ public class TPlan {
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public List<String> getJoinedUsers(){
+		return joinedUsers;
+	}
+	public void setJoinedUsers(List<String> joinedUsers){
+		this.joinedUsers = joinedUsers;
 	}
 	
 	
