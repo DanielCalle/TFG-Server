2bc0f6f99aca89bebb017ef8215cc822ab94f4a8
ZReKoJ <zhong.9745@gmail.com>
2019-04-30 18:12:06 +0200

    added image get to server

---
+++ src/main/java/es/ucm/fdi/tfg/app/controller/ImageController.java
@@ -0,0 +1,42 @@
+package es.ucm.fdi.tfg.app.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.support.ServletContextResource;
+
+@Controller
+@RequestMapping("/images")
+public class ImageController {
+
+    @Autowired
+    private ServletContext servletContext;
+
+    @GetMapping("/{image}")
+    @ResponseBody
+    public ResponseEntity<byte[]> getImageAsByteArray(@PathVariable String image) throws IOException {
+        InputStream in = servletContext.getResourceAsStream("/WEB-INF/images/" + image);
+
+        if (in != null) {
+            return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.IMAGE_JPEG).body(IOUtils.toByteArray(in));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+}
