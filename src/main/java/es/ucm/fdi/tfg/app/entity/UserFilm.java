580dcac005861746b15024c0c722ff2608a18fb0
ZReKoJ <zhong.9745@gmail.com>
2019-05-06 16:10:48 +0200

    create drop db

--- src/main/java/es/ucm/fdi/tfg/app/entity/UserFilm.java
+++ src/main/java/es/ucm/fdi/tfg/app/entity/UserFilm.java
@@ -8,10 +8,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user_film"/*, indexes = {
+@Table(name = "user_film", indexes = {
 	@Index(name = "user_film_user_index", columnList = "user_id"),
 	@Index(name = "user_film_film_index", columnList = "user_id")
-}*/)
+})
 @IdClass(UserFilmId.class)
 public class UserFilm {
 
