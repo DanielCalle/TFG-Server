aab4689e24fbec79cd06f629736614e55314aaed
ZReKoJ <zhong.9745@gmail.com>
2019-05-29 16:15:26 +0200

    test database script

--- database.sh
+++ database.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
+echo start postgresql
 service postgresql start
+
+echo user postgresql
 sudo -i -u postgres
+
+echo open psql
 psql
 \password
+
+echo enter pass
 admin
+
+echo create database 
 create database films;
+
+echo exit
 \q
+
+echo import
 psql -d films -f /usr/src/app/src/main/webapp/WEB-INF/sql/filmar_low.sql
+
 exit
