cdc9f49e8749916312c8a174a91c77aeab7d55b6
ZReKoJ <zhong.9745@gmail.com>
2019-05-29 17:21:30 +0200

    fix errors

--- database.sh
+++ database.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 echo Starting postgresql ...
+
 service postgresql start
+
+sudo -u postgres createdb films
 
 sudo -u postgres psql -U postgres -d films -c "alter user postgres with password 'admin';"
 
-sudo -u postgres createdb films
 sudo -u postgres psql films < /usr/src/app/src/main/webapp/WEB-INF/sql/filmar_low.sql
+
 echo Finished.
 
 exit
